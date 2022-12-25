c433d980082d94e7c9713d21ac29cc0f6c4928c4
Arjunr7019 <67866031+Arjunr7019@users.noreply.github.com>
2022-12-25 17:39:29 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-var icon = document.getElementById("icon");
-var changeMode = document.getElementById("imgChange");
-var changeUserIcon = document.getElementById("changeUr");
-var modeTheme = document.getElementById("theme");
+
+// --------DARK-MODE---------
+
+let icon = document.getElementById("icon");
+let changeMode = document.getElementById("imgChange");
+let changeUserIcon = document.getElementById("changeUr");
+let modeTheme = document.getElementById("theme");
 
 
 icon.onclick = function(){
@@ -16,7 +19,10 @@ icon.onclick = function(){
         changeUserIcon.classList.replace("change-img-ur","img-ur");
         modeTheme.href = "projectsLightMode.html"
     }
-}
+};
+
+
+// --------MOBILE-MODE-NAV-BAR----------
 
 const navMenu = document.querySelector("#navMenu");
 let navMenuVisible = document.querySelector("#navMenuVisible");
@@ -30,4 +36,36 @@ navMenu.addEventListener("click",() => {
     }else{
         navMenuVisible.classList.replace("navVisible","navStyle");
     }
+});
+
+
+// -------User icon and Name color change on mouseover & mouseover--------
+
+let main = document.getElementById("userIcon");
+let userIcon = document.querySelector(".txt-ur").style.color;
+let usrIconChange = document.querySelector("#changeUr").style.background;
+
+main.addEventListener("mouseover", () =>{
+    document.querySelector(".txt-ur").style.color = '#ffc107';
+    document.querySelector("#changeUr").style.background = "bottom / contain no-repeat url('img/profile-yellow.png')";
+});
+main.addEventListener("mouseout", () =>{
+    document.querySelector(".txt-ur").style.color = userIcon;
+    document.querySelector("#changeUr").style.background = usrIconChange;
+});
+
+
+// -------Resume download iocn and text color change on mouseover & mouseover--------
+
+let mainCV = document.getElementById("cvMain");
+let cvDownloadImage = document.querySelector("#cvImage").src;
+let cvDownloadText = document.querySelector("#cvText").style.color;
+
+mainCV.addEventListener("mouseover",()=>{
+    document.querySelector("#cvImage").src = "img/document-icon-yellow.png";
+    document.querySelector("#cvText").style.color = '#ffc107';
+});
+mainCV.addEventListener("mouseout",()=>{
+    document.querySelector("#cvImage").src = cvDownloadImage;
+    document.querySelector("#cvText").style.color = cvDownloadText;
 });
